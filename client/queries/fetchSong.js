723e74988904cda13ab6b6ee2f093dd3acb70aa9
juliusCho <johncrist2000@gmail.com>
2020-10-31 12:28:14 +0900

    study

---
+++ client/queries/fetchSong.js
@@ -0,0 +1,15 @@
+import gql from 'graphql-tag';
+
+export default gql`
+    query FindSongById($id: ID!) {
+        song(id: $id) {
+            id
+            title
+            lyrics {
+                id
+                likes
+                content
+            }
+        }
+    }
+`;
